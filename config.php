43449e4a93a787a6c0279895ca41031596c49830
ThaNerd <thanerd@gmail.com>
2017-04-06 10:37:14 +0200

    Initial upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Yay! Configuration! */
+
+$tnp_database_host = "127.0.0.1";
+$tnp_database_port = 3306;
+$tnp_database_db = "ThaNerdParts";
+$tnp_database_user = "thanerdparts";
+$tnp_database_password = "pstnijkl";
+
+
+?>
---
+++ tnp-load.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+
+if ( file_exists( ABSPATH . 'config.php') ) {
+
+	/** The config file resides in ABSPATH */
+	require_once( ABSPATH . 'config.php' );
+
+}?>
